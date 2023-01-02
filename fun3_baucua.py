fdfc41aef1189048998c92efc77e84906064139f
nghiadoan276 <119665384+nghiadoan276@users.noreply.github.com>
2023-01-02 15:17:52 +0700

    Update fun3_baucua.py

--- fun3_baucua.py
+++ fun3_baucua.py
@@ -1,125 +1,67 @@
 from random import randint as ran
+
+def convert_number_to_string(num):
+    if num == 1:
+        return 'Cop'
+    elif num == 2:
+        return 'Bau'
+    elif num == 3:
+        return 'Ga'
+    elif num == 4:
+        return 'Tom'
+    elif num == 5:
+        return 'Ca'
+    else:
+        return 'Cua'
+
+def compare_result(bet, result1, result2, result3):
+    if bet == result1 or bet == result2 or bet == result3:
+        return True
+    return False
+
+def input_players():
+    so_nguoi = int(input('Nhap so nguoi choi: '))
+    tien = [0] * so_nguoi
+    for i in range(so_nguoi):
+        tien[i] = int(input(f'Nhap tien cuoc cua nguoi thu {i+1}: '))
+    return so_nguoi, tien
+
+def input_bets(so_nguoi):
+    cuoc = [''] * so_nguoi
+    for i in range(so_nguoi):
+        cuoc[i] = input(f'Moi nguoi choi {i+1} cuoc:')
+    return cuoc
+
 print('___Bau Cua___')
 print('Nha cai: Nghiax Doan')
 print('----------')
-#Players
-so_nguoi = int(input('Nhap so nguoi choi: '))
-print('----------')
-tien = [0]*so_nguoi
-for i in range(so_nguoi):
-    tien[i] = int(input(f'Nhap tien cuoc cua nguoi thu {i+1}: '))
+
+so_nguoi, tien = input_players()
 print('----------')
 print('Vay tai khoan cua cac nguoi choi la: ')
 for i in range(so_nguoi):
     print(f'Nguoi {i+1} co {tien[i]} dong')
 print('----------')
-cuoc = ['']*so_nguoi
-for i in range(so_nguoi):
-    cuoc[i] = input(f'Moi nguoi choi {i+1} cuoc:')
+
+cuoc = input_bets(so_nguoi)
 print('----------')
+
 #Computer
-xuc_xac1 = ran(1,6)
-xuc_xac2 = ran(1,6)
-xuc_xac3 = ran(1,6)
-if xuc_xac1 == 1:
-    xuc_xac1 = 'Cop'
-elif xuc_xac1 == 2:
-    xuc_xac1 = 'Bau'
-elif xuc_xac1 == 3:
-    xuc_xac1 = 'Ga'
-elif xuc_xac1 == 4:
-    xuc_xac1 = 'Tom'
-elif xuc_xac1 == 5:
-    xuc_xac1 = 'Ca'
-else:
-    xuc_xac1 = 'Cua'
+xuc_xac1 = convert_number_to_string(ran(1,6))
+xuc_xac2 = convert_number_to_string(ran(1,6))
+xuc_xac3 = convert_number_to_string(ran(1,6))
 
-if xuc_xac2 == 1:
-    xuc_xac2 = 'Cop'
-elif xuc_xac2 == 2:
-    xuc_xac2 = 'Bau'
-elif xuc_xac2 == 3:
-    xuc_xac2 = 'Ga'
-elif xuc_xac2 == 4:
-    xuc_xac2 = 'Tom'
-elif xuc_xac2 == 5:
-    xuc_xac2 = 'Ca'
-else:
-    xuc_xac2 = 'Cua'
-    
-if xuc_xac3 == 1:
-    xuc_xac3 = 'Cop'
-elif xuc_xac3 == 2:
-    xuc_xac3 = 'Bau'
-elif xuc_xac3 == 3:
-    xuc_xac3 = 'Ga'
-elif xuc_xac3 == 4:
-    xuc_xac3 = 'Tom'
-elif xuc_xac3 == 5:
-    xuc_xac3 = 'Ca'
-else:
-    xuc_xac3 = 'Cua'
-
-print('Ket qua xuc xac 1: ',xuc_xac1)
-print('Ket qua xuc xac 2: ',xuc_xac2)
-print('Ket qua xuc xac 3: ',xuc_xac3)
+print('Ket qua xuc xac 1: ', xuc_xac1)
+print('Ket qua xuc xac 2: ', xuc_xac2)
+print('Ket qua xuc xac 3: ', xuc_xac3)
 print('----------')
-#Compare
+
 for i in range(so_nguoi):
-    if cuoc[i] == 'Cop':
-        if xuc_xac1 == 'Cop':
-            tien[i]+=tien[i]
-        elif xuc_xac2 == 'Cop':
-            tien[i]+=tien[i]
-        elif xuc_xac3 == 'Cop':
-            tien[i]+=tien[i]
-        else:
-            tien[i] = 0
-    elif cuoc[i] == 'Bau':
-        if xuc_xac1 == 'Bau':
-            tien[i]+=tien[i]
-        elif xuc_xac2 == 'Bau':
-            tien[i]+=tien[i]
-        elif xuc_xac3 == 'Bau':
-            tien[i]+=tien[i]
-        else:
-            tien[i] = 0
-    elif cuoc[i] == 'Ga':
-        if xuc_xac1 == 'Ga':
-            tien[i]+=tien[i]
-        elif xuc_xac2 == 'Ga':
-            tien[i]+=tien[i]
-        elif xuc_xac3 == 'Ga':
-            tien[i]+=tien[i]
-        else:
-            tien[i] = 0
-    elif cuoc[i] == 'Tom':
-        if xuc_xac1 == 'Tom':
-            tien[i]+=tien[i]
-        elif xuc_xac2 == 'Tom':
-            tien[i]+=tien[i]
-        elif xuc_xac3 == 'Tom':
-            tien[i]+=tien[i]
-        else:
-            tien[i] = 0
-    elif cuoc[i] == 'Ca':
-        if xuc_xac1 == 'Ca':
-            tien[i]+=tien[i]
-        elif xuc_xac2 == 'Ca':
-            tien[i]+=tien[i]
-        elif xuc_xac3 == 'Ca':
-            tien[i]+=tien[i]
-        else:
-            tien[i] = 0
-    elif cuoc[i] == 'Cua':
-        if xuc_xac1 == 'Cua':
-            tien[i]+=tien[i]
-        elif xuc_xac2 == 'Cua':
-            tien[i]+=tien[i]
-        elif xuc_xac3 == 'Cua':
-            tien[i]+=tien[i]
-        else:
-            tien[i] = 0
-print('Sau khi so sanh, tai khoan cua cac bao thu la:')
+    if compare_result(cuoc[i], xuc_xac1, xuc_xac2, xuc_xac3):
+        tien[i] += tien[i]
+    else:
+        tien[i] = 0
+print('----------')
+print('Vay tai khoan cua cac nguoi choi sau khi choi la: ')
 for i in range(so_nguoi):
     print(f'Tai khoan cua nguoi choi {i+1} la: {tien[i]} dong')
